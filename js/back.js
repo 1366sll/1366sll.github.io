cf539590ef684f82ea4b026d6a146f422aa9c909
1366sll <2609338961@qq.com>
2024-01-07 05:14:32 +0800

    Site updated: 2024-01-07 05:14:31

--- js/back.js
+++ js/back.js
@@ -1,30 +1,38 @@
-// 获取标签
-// 全局背景div
-var web_bg = document.getElementById("web_bg");
-// 公共父级
-var content_inner = document.getElementById("content-inner");
-// 获取Cookies
-// 透明度
-var opacity = Cookies.get("opacity");
-// 背景
-var bg = Cookies.get("bg");
-// 动画
-var animation = Cookies.get("animation");
-// 背景类型
-var type = Cookies.get("type");
-// 声明遍历 用于记录当前color
-// 设置背景
-if (bg) {
-  web_bg.style.background = bg;
-  web_bg.setAttribute("data-type", type);
-  if (animation) {
-    web_bg.style.animation = animation;
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+  localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+  let d = JSON.parse(localStorage.getItem(name));
+  // 过期或有错误返回 0 否则返回数据
+  if (d) {
+      let t = Date.now() - d.time
+      if (t < (time * 60 * 1000) && t > -1) return d.data;
   }
+  return 0;
 }
-function setColor(opacity) {
-  // style="--light_bg_color: rgb(255, 255, 255,.3);--dark_bg_color: rgba(18,18,18,.2);"
-  var light_bg_color = "--light_bg_color: rgb(255, 255, 255," + opacity + ");";
-  var dark_bg_color = "--dark_bg_color: rgba(18,18,18," + opacity + ");";
-  content_inner.setAttribute("style", light_bg_color + dark_bg_color);
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+  let data = loadData('blogbg', 1440)
+  if (data) changeBg(data, 1)
+  else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+  let bg = document.getElementById('web_bg')
+  if (s.charAt(0) == '#') {
+      bg.style.backgroundColor = s
+      bg.style.backgroundImage = 'none'
+  } else bg.style.backgroundImage = s
+  if (!flag) { saveData('blogbg', s) }
 }
-setColor(opacity);
