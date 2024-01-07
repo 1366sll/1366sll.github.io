69be5594a7a1dfe2c2d75adf0e5a64339aa72481
1366sll <2609338961@qq.com>
2024-01-08 01:10:27 +0800

    Site updated: 2024-01-08 01:10:25

---
+++ js/bb.js
@@ -0,0 +1,10 @@
+if (document.querySelector('#bber-talk')) {
+    var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical', 
+      loop: true,
+      autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true
+    },
+    });
+  }
