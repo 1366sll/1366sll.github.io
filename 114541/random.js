f92aba0e965d9f1344560ae2469efba5b28128de
1366sll <2609338961@qq.com>
2025-08-26 03:00:17 +0800

    Site updated: 2025-08-26 03:00:16

--- 114541/random.js
+++ 114541/random.js
@@ -1,1 +1,1 @@
-var posts=["2024/08/09/cdn/","2025/08/25/hello-world/","2024/01/01/webui/","2023/08/08/eu_eng/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/08/eu_eng/","2025/08/25/hello-world/","2024/08/09/cdn/","2024/01/01/webui/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
